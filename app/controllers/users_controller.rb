19911151461751324781a7122b2b244b8d0f3a55
masahirokusaka <masahiro820203@gmail.com>
2017-04-22 10:22:29 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,17 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
   def index
-    @users = User.all.page(params[:page])
+     @users = User.all.page(params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    if @user = current_user
     @tasks = @user.tasks.order('created_at DESC').page(params[:page])
     counts @user
+    else
+      render root_pa
+    end
   end
 
   def new
